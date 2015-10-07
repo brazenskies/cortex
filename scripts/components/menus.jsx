dfbd8c0df7a48742d1a5a11a2a44b8fff050f1db
Greg Young <greg.young@hogarthww.com>
2015-10-07 09:15:34 +0100

    Remove redundant files

--- scripts/components/menus.jsx
+++ scripts/components/menus.jsx
@@ -1,8 +1,7 @@
 /** @jsx React.DOM */
 define(function (require) {
     var React = require('react'),
-        CoreMenu = require('jsx!components/core-menu'),
-        DOMUtils = require("jsx!utils/DOMUtils");
+        CoreMenu = require('jsx!components/core-menu');
 
     React.render(<CoreMenu />, document.querySelector('.menu-core'));
 });
